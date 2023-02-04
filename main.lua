00e67d586c034eaafb22fc67204568657ae2dd98
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-02-04 15:47:01 +1030

    Submix

--- main.lua
+++ main.lua
@@ -1,10 +1,18 @@
+local useSubmix = false
+
 RegisterCommand("+carMegaphone", function()
     if canUseCarMegaphone() then
+        if useSubmix then
+            TriggerServerEvent("jay-audio:server:addSubmix")
+        end
         exports["pma-voice"]:overrideProximityRange(50.0, true)
     end
 end)
 RegisterCommand("-carMegaphone", function()
     if canUseCarMegaphone() then
+        if useSubmix then
+            TriggerServerEvent("jay-audio:server:removeSubmix")
+        end
         exports["pma-voice"]:clearProximityOverride()
     end
 end)
